088d0ff982f653988e5ed2148f58a5d50dff7f32
ossahoo-mirp <ossahoo-mirp@mit.edu>
2017-05-31 12:38:18 +0530

    Add files via upload

---
+++ my_file200.cpp
@@ -0,0 +1,14 @@
+include<iostream>
+using namespace std;
+int main ()
+int N, P;
+cout <<"Enter a number: " <<endl;
+cin >> n;
+while (i<=10){
+p=N*1
+cout << "Product upto 10: " << p << endl;
+i++;
+}
+
+return 0;
+}
