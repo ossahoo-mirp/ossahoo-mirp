073425a2f15f3869ed1cd5cf9eda38240764c6a2
ossahoo-mirp <ossahoo-mirp@mit.edu>
2017-05-31 12:24:10 +0530

    Add files via upload

---
+++ my_file12.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+int N, k=0, sum = 0;
+cout<<"Enter a number: "<<endl;
+cin >> N;
+while (k+=N){
+sum+=k;
+k=2;
+}
+cout<<"Sum of all even numbers uptil " << N <<" is "<<sum<<endl;
+
+return 0;
+} 
+
